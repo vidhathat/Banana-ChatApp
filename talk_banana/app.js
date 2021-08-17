b068994dc2f36812695720fdbdf39fdd2ae7589d
Vidhatha T <44058606+vidhathat@users.noreply.github.com>
2021-08-17 11:19:08 +0530

    A simple chat app using fun translation API and Vanilla js

---
+++ talk_banana/app.js
@@ -0,0 +1,12 @@
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+console.log(txtInput);
+
+function clickHandler(){
+    outputDiv.innerText = "translated" + txtInput.value
+;};
+
+btnTranslate.addEventListener("click", clickHandler);
